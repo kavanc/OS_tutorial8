968edb693f31811319f17ca0014249ad765aab48
kavanc <kavanchandra@outlook.com>
2021-03-23 10:32:27 -0400

    for nate

--- q1.c
+++ q1.c
@@ -14,21 +14,46 @@ typedef struct proc
     int priority;
     int memory;
 
-};
+}proc;
 
-struct node
+typedef struct node
 {
-    int key_value;
+    proc process;
     struct node *left;
     struct node *right;
-};
+} node_t;
+
+
+void insert(node_t *tree, proc process){
+    
+
+}
 
 
 int main(void) {
     FILE *fp;
+    char s[2] = ",";
+    char line[256];
+    node_t *tree = (node_t*) malloc(sizeof(node_t));
+    
+    int p_pid;
+
     fp = fopen("processes_tree.txt", "r");
-    if(fp == NULL){
+
+    if(!fp){
         printf("Error!");   
         exit(1);      
     }
+
+    while(fgets(line, sizeof(line), fp)){
+        proc p;
+        strcpy(p.parent, strtok(line, s));
+        strcpy(p.name, strtok(NULL,s));
+        p.priority = atoi(strtok(NULL,s));
+        p.memory = atoi(strtok(NULL, s));
+        insert(tree, p);
+
+
+    }
+   
 } 
